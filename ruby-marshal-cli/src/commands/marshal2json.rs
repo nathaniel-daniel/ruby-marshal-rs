54449becc1fd819383e4dadeed85bd870e6eaa72
Nathaniel Daniel <nathaniel.daniel12@gmail.com>
2023-10-26 20:31:28 -0700

    Update CLI

--- ruby-marshal-cli/src/commands/marshal2json.rs
+++ ruby-marshal-cli/src/commands/marshal2json.rs
@@ -1,5 +1,7 @@
 use anyhow::bail;
+use anyhow::ensure;
 use anyhow::Context;
+use base64::Engine;
 use std::path::PathBuf;
 
 #[derive(Debug, argh::FromArgs)]
@@ -14,11 +16,23 @@ pub struct Options {
 
     #[argh(positional, description = "the output file path")]
     pub output: PathBuf,
+
+    #[argh(
+        switch,
+        long = "convert-binary-strings-to-base64",
+        description = "convert binary strings to base64"
+    )]
+    pub convert_binary_strings_to_base64: bool,
+}
+
+struct ConvertOptions {
+    convert_binary_strings_to_base64: bool,
 }
 
 fn ruby2json_value(
     arena: &ruby_marshal::ValueArena,
     handle: ruby_marshal::ValueHandle,
+    options: &ConvertOptions,
 ) -> anyhow::Result<serde_json::Value> {
     let value = arena.get(handle).context("missing handle")?;
     match value {
@@ -32,10 +46,38 @@ fn ruby2json_value(
 
             let mut array = Vec::with_capacity(value.len());
             for handle in value {
-                array.push(ruby2json_value(arena, *handle)?);
+                array.push(ruby2json_value(arena, *handle, options)?);
             }
 
             Ok(serde_json::Value::Array(array))
+        }
+        ruby_marshal::Value::String(value) => {
+            let instance_variables = value.instance_variables();
+            let encoding = instance_variables.and_then(|instance_variables| {
+                instance_variables.iter().find_map(|(key, value)| {
+                    let name = arena.get_symbol(*key)?.value();
+                    let value = arena.get(*value)?;
+
+                    if name == b"encoding" || name == b"E" {
+                        Some(value)
+                    } else {
+                        None
+                    }
+                })
+            });
+
+            match encoding {
+                Some(_encoding) => {
+                    bail!("cannot convert a String to Json")
+                }
+                None => {
+                    ensure!(options.convert_binary_strings_to_base64, "cannot convert a binary String to Json. Consider using the \"--convert-binary-strings-to-base64\" switch.");
+
+                    Ok(serde_json::Value::String(
+                        base64::engine::general_purpose::STANDARD.encode(value.value()),
+                    ))
+                }
+            }
         }
     }
 }
@@ -46,8 +88,14 @@ pub fn exec(options: Options) -> anyhow::Result<()> {
     let value_arena = ruby_marshal::load(&*file)
         .with_context(|| format!("failed to parse file at \"{}\"", options.input.display()))?;
 
-    let json_value =
-        ruby2json_value(&value_arena, value_arena.root()).context("failed to convert to json")?;
+    let json_value = ruby2json_value(
+        &value_arena,
+        value_arena.root(),
+        &ConvertOptions {
+            convert_binary_strings_to_base64: options.convert_binary_strings_to_base64,
+        },
+    )
+    .context("failed to convert to json")?;
     let output_data = serde_json::to_string(&json_value)?;
 
     let output_tmp = nd_util::with_push_extension(&options.output, "tmp");
