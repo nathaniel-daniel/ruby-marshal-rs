74094f0d356d981c603c4f878d8a38afcca652ae
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-03-10 01:53:25 +0000

    deploy: 1274c5a05b0351cd90458d612a0d46ea7205483a

---
+++ crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["ruby_marshal","ruby_marshal_cli"];
---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["ruby_marshal",["",[["convert",[],["from_value.rs"]],["value_arena",[],["value.rs","value_handle.rs"]]],["convert.rs","dump.rs","lib.rs","load.rs","value_arena.rs"]]],\
+["ruby_marshal_cli",["",[["commands",[],["marshal2json.rs"]]],["commands.rs","main.rs"]]]\
+]'));
+createSrcSidebar();
