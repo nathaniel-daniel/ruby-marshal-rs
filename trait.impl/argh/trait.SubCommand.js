e07a9daeb8e3bebeb1557d0f69478bf651ca5dcd
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2024-07-02 08:10:37 +0000

    deploy: f36ba759adf4c932f46fc7c9ceac0b9b9593c181

---
+++ trait.impl/argh/trait.SubCommand.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"ruby_marshal_cli":[["impl SubCommand for <a class=\"struct\" href=\"ruby_marshal_cli/commands/marshal2json/struct.Options.html\" title=\"struct ruby_marshal_cli::commands::marshal2json::Options\">Options</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
