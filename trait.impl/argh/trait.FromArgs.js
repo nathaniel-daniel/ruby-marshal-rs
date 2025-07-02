b39b1b44f6e67d0f5f2844cf1d591c5a0be2d0a5
nathaniel-daniel <nathaniel-daniel@users.noreply.github.com>
2025-07-02 03:33:43 +0000

    deploy: 0c972e76ad1005000a579d64d6236a0d957791d1

---
+++ trait.impl/argh/trait.FromArgs.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ruby_marshal_cli",[["impl FromArgs for <a class=\"enum\" href=\"ruby_marshal_cli/enum.Subcommand.html\" title=\"enum ruby_marshal_cli::Subcommand\">Subcommand</a>"],["impl FromArgs for <a class=\"struct\" href=\"ruby_marshal_cli/commands/marshal2json/struct.Options.html\" title=\"struct ruby_marshal_cli::commands::marshal2json::Options\">Options</a>"],["impl FromArgs for <a class=\"struct\" href=\"ruby_marshal_cli/struct.Options.html\" title=\"struct ruby_marshal_cli::Options\">Options</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[500]}
